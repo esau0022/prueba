b7f80a9f186db07abc93d2871fe4d8a3ea8a5936
esau0022 <esaupltz@gmail.com>
2025-11-19 08:38:52 -0600

    cambio

---
+++ xd.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<:"HOLA"
+    // git branch para ver las ramas 
+    //git branch (nombre) para crear una rama 
+    // git checkout (nombre) para moverte a esa rama 
+    return 0;
+}
